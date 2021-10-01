449f12e53ca91a3d4fdfd6598766c45843591831
Sergey Levitin <selevit@gmail.com>
2021-10-01 23:23:28 +0300

    Adjust run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 set -e
 cargo build --release
+killall server || true &>/dev/null
 echo "Running TCP server"
 ./target/release/server &
 server_pid=$!
 set +e
+sleep 0.1
 echo "Running TCP client"
 trap "kill $server_pid" SIGINT
 ./target/release/client
