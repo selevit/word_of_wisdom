010baeb7bd6dd40dd5864f09d54006f92802683b
Sergey Levitin <selevit@gmail.com>
2021-10-02 12:48:57 +0300

    Configurable env

--- src/client.rs
+++ src/client.rs
@@ -3,10 +3,12 @@ use anyhow::Result;
 use env_logger::Env;
 use proof_of_work::Client;
 use std::error::Error;
+use std::env;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let addr = env::var("SERVER_ADDR").unwrap_or_else(|_| "127.0.0.1:4444".into());
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let client = Client::new("127.0.0.1:4444");
+    let client = Client::new(&addr);
     match client.get_response() {
         Ok(r) => {
             log::info!("Server response: > {}", r);
--- src/server.rs
+++ src/server.rs
@@ -2,11 +2,12 @@ pub mod proof_of_work;
 use env_logger::Env;
 use proof_of_work::Server;
 use std::error::Error;
-
-const RESPONSES_FILENAME: &str = "server_responses.txt";
+use std::env;
 
 fn main() -> Result<(), Box<dyn Error>> {
+    let responses_file = env::var("RESPONSES_FILE").unwrap_or_else(|_| "./server_responses.txt".into());
+    let addr = env::var("LISTEN_ADDR").unwrap_or_else(|_| "127.0.0.1:4444".into());
     env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
-    let server = Server::new_from_file(RESPONSES_FILENAME)?;
-    server.run("127.0.0.1:4444")
+    let server = Server::new_from_file(&responses_file)?;
+    server.run(&addr)
 }
